a34ee6f87c1a1cc9024ce81ab8e51205be4865af
RaulVanegas7 <raulvanegas711@gmail.com>
2025-06-23 14:37:04 -0500

    Agregada la entidad de Service

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DentalClinicApi.Models;
+using DentalClinicApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DentalClinicApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServiceController : ControllerBase
+    {
+        private readonly ServiceService _serviceService;
+
+        public ServiceController(ServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Service>>> GetAll()
+        {
+            var services = await _serviceService.GetAllServices();
+            return Ok(services);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Service>> GetById(string id)
+        {
+            var service = await _serviceService.GetOneById(id);
+            if (service == null)
+                return NotFound();
+
+            return Ok(service);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] Service newService)
+        {
+            await _serviceService.CreateService(newService);
+            return CreatedAtAction(nameof(GetById), new { id = newService.Id }, newService);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(string id, Service updateService)
+        {
+            var findService = await _serviceService.GetOneById(id);
+            if (findService == null)
+                return NotFound();
+
+            updateService.Id = id;
+            await _serviceService.UpdateService(id, updateService);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var findService = await _serviceService.GetOneById(id);
+            if (findService == null)
+                return NotFound();
+
+            await _serviceService.DeleteService(id);
+            return NoContent();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ builder.Services.Configure<DataBaseSettings>(
 builder.Services.AddSingleton<MongoDbContext>();
 builder.Services.AddSingleton<PatientService>();
 builder.Services.AddSingleton<DentistService>();
+builder.Services.AddSingleton<ServiceService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
