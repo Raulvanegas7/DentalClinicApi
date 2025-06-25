06eff2b3c6cf5aa92818b1bd28b5085189f349fa
RaulVanegas7 <raulvanegas711@gmail.com>
2025-06-24 21:40:09 -0500

    Configuracion de middleware en Program

--- Program.cs
+++ Program.cs
@@ -79,6 +79,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<DentalClinicApi.Middlewares.ErrorHandlingMiddleware>();
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
