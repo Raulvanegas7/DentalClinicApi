6300924f071de89635f0ceab320a9ccd475d7ef0
RaulVanegas7 <raulvanegas711@gmail.com>
2025-07-23 14:34:49 -0500

    Cambio de prueba

--- backend/Dtos/UpdatePatientDto.cs
+++ backend/Dtos/UpdatePatientDto.cs
@@ -9,9 +9,9 @@ namespace backend.Dtos
     {
         public string Name { get; set; } 
 
-        public string Email { get; set; } 
+        public string Email { get; set; }
 
-        public string Phone { get; set; }
+        public string Phone { get; set; } = string.Empty;
 
         public string Address { get; set; }
 
